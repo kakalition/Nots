af2b4475518b5c27a2ea742d74293ecda3196451
Kaka <kakalition@gmail.com>
2022-01-29 16:49:19 +0700

    implement title edit text

--- app/src/main/java/com/daggery/nots/home/view/AddEditNoteFragment.kt
+++ app/src/main/java/com/daggery/nots/home/view/AddEditNoteFragment.kt
@@ -5,18 +5,32 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
+import com.daggery.nots.NotsApplication
 import com.daggery.nots.R
+import com.daggery.nots.databinding.FragmentAddEditNoteBinding
+import com.daggery.nots.home.viewmodel.HomeViewModel
+import com.daggery.nots.home.viewmodel.HomeViewModelFactory
 
 // TODO: Implement Save Edit Functionality
 
 class AddEditNoteFragment : Fragment() {
 
+    private var _binding: FragmentAddEditNoteBinding? = null
+    private val binding get() = _binding!!
+
+    private val viewModel: HomeViewModel by activityViewModels {
+        HomeViewModelFactory(
+            (this.activity?.application as NotsApplication).database
+        )
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_edit_note, container, false)
+        _binding = FragmentAddEditNoteBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
