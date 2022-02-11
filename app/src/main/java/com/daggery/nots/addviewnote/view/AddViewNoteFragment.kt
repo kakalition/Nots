190db559d0f0f4ab4e202bba1a1cc1ab756fd691
Kaka <kakalition@gmail.com>
2022-02-11 22:08:14 +0700

    initial success implementing editor behavior

--- app/src/main/java/com/daggery/nots/addviewnote/view/AddViewNoteFragment.kt
+++ app/src/main/java/com/daggery/nots/addviewnote/view/AddViewNoteFragment.kt
@@ -1,10 +1,14 @@
 package com.daggery.nots.addviewnote.view
 
+import android.content.res.Resources
 import android.graphics.Color
+import android.graphics.Rect
 import android.os.Bundle
 import android.text.Editable
 import android.util.Log
 import android.view.*
+import android.widget.ResourceCursorAdapter
+import android.widget.ScrollView
 import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
@@ -13,10 +17,12 @@ import com.daggery.nots.R
 import com.daggery.nots.addviewnote.utils.AddViewNoteFragmentUtils
 import com.daggery.nots.addviewnote.viewmodel.AddViewNoteViewModel
 import com.daggery.nots.databinding.FragmentAddViewNoteBinding
+import com.google.android.material.color.MaterialColors
 import com.google.android.material.transition.MaterialContainerTransform
 import dagger.hilt.android.AndroidEntryPoint
+import hilt_aggregated_deps._com_daggery_nots_addviewnote_viewmodel_AddViewNoteViewModel_HiltModules_BindsModule
 
-// TODO: Check Observer
+
 // TODO: Handle Long Text Body, possibly solved by using linear layout
 
 data class UneditedNote(
@@ -33,6 +39,8 @@ class AddViewNoteFragment : Fragment() {
     internal val viewModel: AddViewNoteViewModel by activityViewModels()
 
     private val args: AddViewNoteFragmentArgs by navArgs()
+
+    private var screenHeight: Int = 0
 
     private var _fragmentUtils: AddViewNoteFragmentUtils? = null
     private val fragmentUtils get() = _fragmentUtils!!
@@ -72,6 +80,8 @@ class AddViewNoteFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        screenHeight = Resources.getSystem().displayMetrics.heightPixels
+
         _fragmentUtils = AddViewNoteFragmentUtils(this, args)
         _editableFactory = Editable.Factory()
         isNewNote = args.uuid.isBlank()
@@ -87,6 +97,17 @@ class AddViewNoteFragment : Fragment() {
             }
         }
 
+        // TODO: Clean this things up
+        // TODO: Maybe I can use invisible view that capture touch and focus on edit text
+        var availHeight = 0f
+        viewBinding.appBarFrame.post {
+            availHeight += viewBinding.noteBody.y
+        }
+
+        viewBinding.noteBody.post {
+            availHeight += viewBinding.noteBody.y
+            viewBinding.noteBody.minHeight = (screenHeight - availHeight).toInt()
+        }
     }
 
     override fun onDestroyView() {
@@ -100,7 +121,13 @@ class AddViewNoteFragment : Fragment() {
         return MaterialContainerTransform().apply {
             duration = 700
             scrimColor = Color.TRANSPARENT
-            setAllContainerColors(requireContext().getColor(R.color.black_surface))
+            setAllContainerColors(
+                MaterialColors.getColor(
+                    requireContext(),
+                    com.google.android.material.R.attr.colorSurface,
+                    Color.parseColor("#FF212121")
+                )
+            )
             drawingViewId = R.id.fragment_container_view
         }
     }
