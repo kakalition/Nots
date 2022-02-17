da0351c7d06139473873b8312029882b04a33cf0
Kaka <kakalition@gmail.com>
2022-02-17 16:33:13 +0700

    add menu item and adjust ic_add size

--- app/src/main/java/com/daggery/nots/home/utils/FilterFragmentUtils.kt
+++ app/src/main/java/com/daggery/nots/home/utils/FilterFragmentUtils.kt
@@ -1,8 +1,11 @@
 package com.daggery.nots.home.utils
 
+import android.graphics.Color
+import android.view.MenuItem
 import androidx.navigation.fragment.findNavController
 import com.daggery.nots.R
 import com.daggery.nots.home.view.FilterFragment
+import com.google.android.material.color.MaterialColors
 
 class FilterFragmentUtils(private val fragment: FilterFragment) {
 
@@ -10,8 +13,26 @@ class FilterFragmentUtils(private val fragment: FilterFragment) {
         with(fragment.viewBinding.toolbarBinding.toolbar) {
             setNavigationIcon(R.drawable.ic_back)
             setNavigationOnClickListener { fragment.findNavController().navigateUp() }
+            inflateMenu(R.menu.menu_filter_fragment)
+            menu.findItem(R.id.add_tags_button).icon.setTint(
+                MaterialColors.getColor(
+                    fragment.requireContext(),
+                    com.google.android.material.R.attr.colorOnSurface,
+                    Color.parseColor("#FF212121")
+                )
+            )
+            setOnMenuItemClickListener(menuItemClickListener)
             title = "Tags Filter"
         }
-
     }
+
+    private val menuItemClickListener = { item: MenuItem ->
+        when(item.itemId) {
+            R.id.add_tags_button -> {
+                true
+            }
+            else -> false
+        }
+    }
+
 }
