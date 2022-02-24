25c6682c4cf55315a862c309ce13aad7d2ea4078
Kaka <kakalition@gmail.com>
2022-02-24 20:43:32 +0700

    add required module

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,7 +7,7 @@ pluginManagement {
     plugins {
         kotlin("kapt") version "1.6.10"
         id("com.android.application") version "7.1.1" apply false
-        id("com.android.library") version "7.1.1" apply false
+        id("com.android.library") version "7.1.2" apply false
         id("org.jetbrains.kotlin.android") version "1.6.10" apply false
         id("org.jetbrains.kotlin.jvm") version "1.6.10" apply false
         id("org.gradle.kotlin.kotlin-dsl") version "2.1.7" apply false
@@ -17,3 +17,8 @@ pluginManagement {
 
 rootProject.name = "Nots"
 include(":app", ":data", ":domain")
+include(":features:dashboard")
+include(":features:notes")
+include(":features:tags")
+include(":features:books")
+include(":features:settings")
