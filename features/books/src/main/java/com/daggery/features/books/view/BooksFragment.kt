70dcbf47d1c9dce5406860811a6d768671eb5570
Kaka <kakalition@gmail.com>
2022-02-25 14:04:13 +0700

    Create Book Tile: Books Module

---
+++ features/books/src/main/java/com/daggery/features/books/view/BooksFragment.kt
@@ -0,0 +1,31 @@
+package com.daggery.features.books.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
+import com.daggery.features.books.databinding.BooksFragmentBinding
+
+class BooksFragment : Fragment() {
+
+    private var _viewBinding: BooksFragmentBinding? = null
+    private val viewBinding get() = _viewBinding!!
+
+    private val viewModel: BooksViewModel by activityViewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _viewBinding = BooksFragmentBinding.inflate(inflater, container, false)
+        return viewBinding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+
+        _viewBinding = null
+    }
+}
---
+++ features/books/src/main/java/com/daggery/features/books/view/BooksViewModel.kt
@@ -0,0 +1,7 @@
+package com.daggery.features.books.view
+
+import androidx.lifecycle.ViewModel
+
+class BooksViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
