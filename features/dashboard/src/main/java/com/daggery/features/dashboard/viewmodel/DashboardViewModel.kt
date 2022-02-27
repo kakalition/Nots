28ee5a391bd8354537296d69f8ba83ef96da6aca
Kaka <kakalition@gmail.com>
2022-02-27 08:53:39 +0700

    Scrolling AppBar in Dashboard

--- features/dashboard/src/main/java/com/daggery/features/dashboard/viewmodel/DashboardViewModel.kt
+++ features/dashboard/src/main/java/com/daggery/features/dashboard/viewmodel/DashboardViewModel.kt
@@ -1,7 +1,9 @@
 package com.daggery.features.dashboard.viewmodel
 
 import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
 
+@HiltViewModel
 class DashboardViewModel : ViewModel() {
     private val test = 0
 }
