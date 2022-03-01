5e48160a7380b2ad8f4777a028a573425aa08f66
Kaka <kakalition@gmail.com>
2022-03-01 11:29:57 +0700

    Create Quick Action Tile

--- features/dashboard/src/main/java/com/daggery/features/dashboard/view/DashboardFragment.kt
+++ features/dashboard/src/main/java/com/daggery/features/dashboard/view/DashboardFragment.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.activityViewModels
 import com.daggery.features.dashboard.databinding.FragmentDashboardBinding
+import com.daggery.features.dashboard.databinding.TileQuickActionBinding
 import com.daggery.features.dashboard.viewmodel.DashboardViewModel
 
 class DashboardFragment : Fragment() {
@@ -23,6 +24,15 @@ class DashboardFragment : Fragment() {
         // Inflate the layout for this fragment
         _viewBinding = FragmentDashboardBinding.inflate(inflater, container, false)
         return viewBinding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        TileQuickActionBinding.bind(viewBinding.root).apply {
+            newNote.noteTitle.text = "New Note"
+            newTag.noteTitle.text = "New Tag"
+        }
     }
 
     override fun onDestroyView() {
