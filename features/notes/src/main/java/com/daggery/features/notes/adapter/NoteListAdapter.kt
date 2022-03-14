f0d58cb6d8f63643ff950a8b3d2f4d4089fb770b
Kaka <kakalition@gmail.com>
2022-03-14 07:44:08 +0700

    Change: Adjust chip style

--- features/notes/src/main/java/com/daggery/features/notes/adapter/NoteListAdapter.kt
+++ features/notes/src/main/java/com/daggery/features/notes/adapter/NoteListAdapter.kt
@@ -1,7 +1,6 @@
 package com.daggery.nots.home.adapter
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
